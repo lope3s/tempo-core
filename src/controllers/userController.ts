1546ba80575df1b354843ea56f3f91f8cc599037
Luan Lopes <luan.lopes@clickativo.com.br>
2022-10-12 13:54:04 -0300

    fix: adding validation to provided id and improved security on update method

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -39,6 +39,12 @@ class UserController {
         if (!req.params.id) res.status(400).json({error: 'Missing id.'})
 
         try {
+            new ObjectId(req.params.id)
+        } catch (error) {
+            return res.status(400).json({error: "Invalid ID format."})
+        }
+
+        try {
             const user = await getUser({_id: new ObjectId(req.params.id)})
 
             if (!user) return res.status(404).json({error: 'User not found.'})
@@ -56,19 +62,29 @@ class UserController {
         if (!req.params.id) res.status(400).json({error: 'Missing id.'})
 
         try {
-            const updatedUser = await updateUser(req.params.id, req.body)
+            new ObjectId(req.params.id)
+        } catch (error) {
+            return res.status(400).json({error: "Invalid ID format."})
+        }
 
-            if (!updatedUser) return res.status(404).json({error: 'User not found.'})
+        try {
+            await updateUser(req.params.id, req.body)
 
             return res.status(200).json({message: 'User updated.'})
-        } catch (error) {
+        } catch (error: any) {
             console.log(error)
-            return res.status(500).json({error: 'Internal Server Error.'})
+            return res.status(404).json({error: error.message})
         }
     }
 
     async deleteUser(req: Request, res: Response) {
         if (!req.params.id) res.status(400).json({error: 'Missing id.'})
+
+        try {
+            new ObjectId(req.params.id)
+        } catch (error) {
+            return res.status(400).json({error: "Invalid ID format."})
+        }
 
         try {
             await deleteUser(req.params.id)
